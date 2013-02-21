96ffd0d305d01bc22342af83b102948c8058648c
LordJZ <LordJZ@outlook.com>
2013-02-21 05:02:39 +0400

    Fix projects' Code Contracts

--- LordJZ/Properties/AssemblyInfo.cs
+++ LordJZ/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LordJZ")]
+[assembly: AssemblyTitle("LordJZ's General Purpose Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LordJZ")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("LordJZ's General Purpose Library")]
+[assembly: AssemblyCopyright("Copyright © LordJZ 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
