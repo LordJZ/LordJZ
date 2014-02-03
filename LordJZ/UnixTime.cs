ac4172942212cee353d9729395d0f63ee909098f
LordJZ <LordJZ@outlook.com>
2014-02-03 22:20:21 +0400

    Added simple UnixTime implementation.

---
+++ LordJZ/UnixTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LordJZ
+{
+    public static class UnixTime
+    {
+        public static readonly DateTime EpochUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static readonly DateTime Epoch = TimeZone.CurrentTimeZone.ToLocalTime(EpochUtc);
+
+        public static long ToUnixTime(this DateTime dateTime)
+        {
+            return (long)(dateTime.ToUniversalTime() - Epoch).TotalSeconds;
+        }
+
+        public static DateTime FromNumber(long unixTime)
+        {
+            return Epoch.AddSeconds(unixTime);
+        }
+    }
+}
