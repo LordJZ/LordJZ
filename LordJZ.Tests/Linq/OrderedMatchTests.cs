c3e7aa19bdb43d5e33e75578e5b1865d5aa35aa0
LordJZ <LordJZ@outlook.com>
2014-02-03 22:20:50 +0400

    Implemented OrderedMatch and tests for it.

---
+++ LordJZ.Tests/Linq/OrderedMatchTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LordJZ.Linq;
+
+namespace LordJZ.Tests.Linq
+{
+    [TestClass]
+    public class OrderedMatchTests
+    {
+        static void Test(int[] haystack, int[] needles, int?[] result)
+        {
+            int?[] got = haystack.OrderedMatch(needles, _ => _, _ => _, Comparer<int>.Default,
+                                               (v1, v2, r) =>
+                                               {
+                                                   Assert.AreEqual(r, v2);
+                                                   Assert.AreEqual(r, v1);
+
+                                                   return (int?)r;
+                                               },
+                                               (v, k) => (int?)null)
+                                 .ToArray();
+            Assert.IsTrue(got.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void TestSimpleSequence()
+        {
+            Test(new[] { 1, 2, 3 }, new[] { 2, 3, 4 }, new int?[] { 2, 3, null });
+            Test(new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }, new int?[] { 1, 2, 3, null });
+            Test(new[] { 1, 3, 4 }, new[] { 1, 2, 4 }, new int?[] { 1, null, 4 });
+        }
+
+        [TestMethod]
+        public void TestEmptySequences()
+        {
+            Test(new[] { 1, 2, 3 }, new int[0], new int?[0]);
+            Test(new int[0], new[] { 1, 2, 3, 4 }, new int?[] { null, null, null, null });
+        }
+
+        [TestMethod]
+        public void TestDuplicates()
+        {
+            Test(new[] { 1, 2, 2, 3 }, new[] { 1, 2, 3, 4 }, new int?[] { 1, 2, 3, null });
+            Test(new[] { 1, 2, 2, 3 }, new[] { 1, 2 }, new int?[] { 1, 2 });
+            Test(new[] { 2, 2, 3 }, new[] { 2 }, new int?[] { 2 });
+            Test(new[] { 1, 2, 4 }, new[] { 1, 2, 2, 3, 4 }, new int?[] { 1, 2, null, null, 4 });
+            Test(new[] { 1, 2, 2, 4 }, new[] { 1, 2, 2, 3, 4 }, new int?[] { 1, 2, 2, null, 4 });
+        }
+    }
+}
---
+++ LordJZ/Linq/Enumerable.OrderedMatch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LordJZ.Linq
+{
+    partial class Enumerable
+    {
+        public static IEnumerable<TResult> OrderedMatch<THaystack, TNeedle, TKey, TResult>(
+            this IEnumerable<THaystack> haystack,
+            IEnumerable<TNeedle> needles,
+            Func<THaystack, TKey> haystackKeySelector,
+            Func<TNeedle, TKey> needleKeySelector,
+            IComparer<TKey> keyComparer,
+            Func<THaystack, TNeedle, TKey, TResult> matchedResultSelector,
+            Func<TNeedle, TKey, TResult> mismatchedResultSelector)
+        {
+            IEnumerator<THaystack> eHaystack = haystack.GetEnumerator();
+            bool haystackEmpty;
+            THaystack haystackItem = default(THaystack);
+            TKey haystackKey = default(TKey);
+
+            if (!(haystackEmpty = !eHaystack.MoveNext()))
+            {
+                haystackItem = eHaystack.Current;
+                haystackKey = haystackKeySelector(haystackItem);
+            }
+
+            foreach (TNeedle needle in needles)
+            {
+                TKey needleKey = needleKeySelector(needle);
+
+                for (;;)
+                {
+                    if (haystackEmpty)
+                    {
+                        yield return mismatchedResultSelector(needle, needleKey);
+                        break;
+                    }
+
+                    // check the current haystack element
+                    int comparison = keyComparer.Compare(needleKey, haystackKey);
+
+                    if (comparison >= 0)
+                    {
+                        // needleItem >= haystackItem, always adjust haystack
+
+                        if (comparison == 0)
+                            // found a match
+                            yield return matchedResultSelector(haystackItem, needle, needleKey);
+
+                        // adjust haystack
+                        if (!(haystackEmpty = !eHaystack.MoveNext()))
+                        {
+                            haystackItem = eHaystack.Current;
+                            haystackKey = haystackKeySelector(haystackItem);
+                        }
+
+                        if (comparison > 0)
+                            // not found a match, try another haystack item
+                            continue;
+                    }
+                    else
+                        // needleItem < haystackItem, no haystack item matches this needle
+                        // do not adjust haystack
+                        yield return mismatchedResultSelector(needle, needleKey);
+
+                    break;
+                }
+            }
+        }
+    }
+}
