c734ae0f6c5d25b06b5c656340d6a0b479313382
LordJZ <lordjz@outlook.com>
2014-08-18 22:09:20 +0400

    Added TaskExtensions.FireAndForget.

---
+++ LordJZ/Threading/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace LordJZ.Threading
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Ensures exceptions in unawaited tasks would terminate the process.
+        /// </summary>
+        public static async void FireAndForget(this Task task)
+        {
+            Contract.Requires(task != null);
+
+            await task;
+        }
+    }
+}
